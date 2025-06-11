6cb01397ea261365a1a9ebb80b6544fe3cbeff49
linjacques <linjacques463@gmail.com>
2025-06-11 22:39:04 +0200

    ajout de la structure du Pipeline ET des scripts extract avec les données correspondant

--- projet_analyse_jeux_videos/src/main/scala/main.scala
+++ projet_analyse_jeux_videos/src/main/scala/main.scala
@@ -1,32 +1,20 @@
-package example
-import org.apache.spark.sql.SparkSession
-import com.typesafe.config.ConfigFactory
+package pipelines
 
-object Hello {
+import org.apache.spark.sql.SparkSession
+import pipelines.extract.ExtractFromCSV
+import pipelines.extract.ExtractFromPostgres
+
+object Main {
   def main(args: Array[String]): Unit = {
-    val config = ConfigFactory.load()
-    val pgUrl = config.getString("postgres.url")
-    val pgUser = config.getString("postgres.user")
-    val pgPwd = config.getString("postgres.password")
-    val pgTable = config.getString("postgres.table")
 
     val spark = SparkSession.builder()
-      .appName("Spark JDBC")
+      .appName("VideoGameETL")
       .master("local[*]")
-      .config("spark.driver.host", "127.0.0.1") 
+      .config("spark.driver.host", "127.0.0.1")
       .getOrCreate()
 
-    val df = spark.read
-      .format("jdbc")
-      .option("url", pgUrl)
-      .option("dbtable", pgTable)
-      .option("user", pgUser)
-      .option("password", pgPwd)
-      .option("driver", "org.postgresql.Driver")
-      .load()
-
-      df.show(10)
-
+    ExtractFromCSV.run(spark)
+    ExtractFromPostgres.run(spark)
 
     spark.stop()
   }
