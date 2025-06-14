ede6676151f42cce337b5f94b378cadedf8b76da
linjacques <linjacques463@gmail.com>
2025-06-14 22:07:57 +0200

    merge de la branche argent dans main

--- projet_analyse_jeux_videos/src/main/scala/main.scala
+++ projet_analyse_jeux_videos/src/main/scala/main.scala
@@ -3,6 +3,7 @@ package pipelines
 import org.apache.spark.sql.SparkSession
 import pipelines.extract.ExtractFromCSV
 import pipelines.extract.ExtractFromPostgres
+import pipelines.transform.silver
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -15,6 +16,7 @@ object Main {
 
     ExtractFromCSV.run(spark)
     ExtractFromPostgres.run(spark)
+    silver.silver_layer(spark)
 
     spark.stop()
   }
